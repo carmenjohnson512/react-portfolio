a0481db9b3dd3acd06c4500a62f360e81456acae
Carmen Johnson <carmenjohnson512@gmail.com>
2020-10-11 01:32:57 -0500

    adding heroku build files

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/carmen.johnson/UT-FSF-BOOTCAMP/react-portfolio/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/carmen.johnson/UT-FSF-BOOTCAMP/react-portfolio/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/carmen.johnson/UT-FSF-BOOTCAMP/react-portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/carmen.johnson/UT-FSF-BOOTCAMP/react-portfolio/src/pages/index.js")))
 }
